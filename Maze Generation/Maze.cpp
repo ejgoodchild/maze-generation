e0f9ba8a726225de25efcfe4db8ae4f83e93831c
ejgoodchild <32480163+ejgoodchild@users.noreply.github.com>
2020-10-31 15:53:28 +0000

    Path Generator

--- Maze Generation/Maze.cpp
+++ Maze Generation/Maze.cpp
@@ -1,4 +1,8 @@
 #include "Maze.h"
+#include <algorithm>
+
+
+
 
 Maze::Maze(int w, int h)
 {
@@ -32,7 +36,9 @@ void Maze::printMaze()
 void Maze::generateMaze()
 {
 	generateNodes();
+	generatePaths();
 	generateStartPoint();
+	generateWalls();
 }
 
 void Maze::generateNodes()
@@ -48,18 +54,18 @@ void Maze::generateNodes()
 	}
 }
 
+
+
+
+
 void Maze::generateStartPoint()
 {
-	
-	
-	int ch = (height - 1) / 2;
-	int cw = (width - 1) / 2;
-	
+	MazeNode* startPoint = getStartNode();
 	MazeNode* mNode;
-	for (int y = ch - 1; y < ch+ 1; y++) {
-		for (int x = cw - 1; x < cw + 1; x++) {
+	for (int y = startPoint->y - 1; y <= startPoint->y + 1; y++) {
+		for (int x = startPoint->x - 1; x <= startPoint->x + 1; x++) {
 			mNode = &nodes.at(getNodesPos(x, y));
-			mNode->nodeType = (y == ch && x == cw) ? 'S' : ' ';
+			mNode->nodeType = mNode == startPoint ? 'S' : ' ';
 		
 
 		}
@@ -68,6 +74,82 @@ void Maze::generateStartPoint()
 	
 
 
+}
+
+void Maze::generateWalls()
+{
+	MazeNode* mNode;
+	for (int y = 0; y < height; y++) {
+		for (int x = 0; x < width; x++) {
+			mNode = &nodes.at(getNodesPos(x, y));
+			if(mNode->nodeType == NULL){
+				mNode->nodeType = 'X';
+				mNode->passable = false;
+			}
+
+			
+			
+		}
+	}
+}
+void Maze::generatePaths(MazeNode* curNode)
+{
+	Directions dir[]{ Directions::NORTH, Directions::EAST, Directions::SOUTH, Directions::WEST };
+	MazeNode* endNode = NULL;
+
+	std::random_shuffle(std::begin(dir), std::end(dir));
+	for (int i = 0; i < 4; i++) {
+		endNode = getPathEndNode(curNode, dir[i]);
+		if (endNode != NULL) {
+			//endNode->print();
+			fillPathNodes(curNode, endNode);
+			generatePaths(endNode);
+		}				
+	}
+}
+
+
+MazeNode* Maze::getPathEndNode(MazeNode* curNode, Directions dir)
+{
+	int x = 0, y = 0;
+	
+	switch (dir) {
+	case Directions::NORTH:
+		y = curNode->y - 2 <= 0 ? 0 : -2;
+		break;
+	case Directions::EAST:
+		x = curNode->x + 2 >= width - 1 ? 0 : 2;
+		break;
+
+	case Directions::SOUTH:
+		y = curNode->y + 2 >= height - 1 ? 0 : 2;
+		break;
+
+	case Directions::WEST:
+		x = curNode->x - 2 <= 0 ? 0 : -2;
+		break;
+
+	}
+
+	MazeNode* endNode = &nodes.at(getNodesPos(curNode->x + x, curNode->y + y));
+	endNode = endNode->nodeType ? NULL : endNode;
+
+	return curNode == endNode ? NULL : endNode ;
+}
+
+void Maze::fillPathNodes(MazeNode* curNode, MazeNode* nextNode)
+{
+
+	for (int x = curNode->x; x != nextNode->x; x += curNode->x > nextNode->x ? -1 : 1) {
+		
+		nodes.at(getNodesPos(x, curNode->y)).nodeType = ' ';
+
+	}
+	for (int y = curNode->y; y != nextNode->y; y += curNode->y > nextNode->y ? -1 : 1) {
+
+		nodes.at(getNodesPos(curNode->x, y)).nodeType = ' ';
+	}
+	
 }
 
 
--- Maze Generation/Maze.h
+++ Maze Generation/Maze.h
@@ -3,12 +3,13 @@
 #include <vector>
 
 using namespace std;
-
+enum class Directions {NORTH = 0, EAST = 1, SOUTH = 2, WEST = 3};
 struct MazeNode {
 	int x, y;
 	char nodeType = NULL;
 	float g, h;
 	bool passable = true;
+	void print() { cout << "[" <<x <<"," << y << "]"; };
 };
 class Maze
 {
@@ -28,10 +29,30 @@ class Maze
 		int edges = 1;
 		int getNodesPos(int x, int y){
 			return x + (y * width);
-		}
+		}	
+
 		std::vector <MazeNode> nodes;
+		
+		
 		void generateNodes();
+		void generatePaths() {
+			generatePaths(getStartNode());
+		};
+		void generatePaths(MazeNode* node);
+
+
 		void generateStartPoint();
+		void generateWalls();
+
+		MazeNode* getStartNode(){
+			return &nodes.at(getNodesPos((width - 1) / 2, (height - 1) / 2));
+		}
+
+
+		
+
+		MazeNode* getPathEndNode(MazeNode*, Directions);
+		void fillPathNodes(MazeNode*, MazeNode*);
 
 };
 
